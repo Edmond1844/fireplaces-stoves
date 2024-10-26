90b5a34526a6579847e2cd5cece16b813da4df0c
Edmond1844 <pavel.rudenko1844@gmail.com>
2024-10-26 16:29:16 +0300

    Объекты

--- source/js/script.js
+++ source/js/script.js
@@ -1,155 +1,109 @@
-// Header
+// header !!!
 
-let header =  document.createElement('header');
+// Объекты header
+let headerBtn = {
+    text: 'Каталог'
+};
+
+let headerLinks = {
+    link1: {
+        linkText: 'Дымоходы',
+        linkUrl: '#'
+    },
+    link2: {
+        linkText: 'Монтаж',
+        linkUrl: '#'
+    },
+    link3: {
+        linkText: 'Портфолио',
+        linkUrl: '#'
+    },
+    link4: {
+        linkText: 'Доставка и оплата',
+        linkUrl: '#'
+    },
+    link5: {
+        linkText: 'Отзывы',
+        linkUrl: '#'
+    },
+    link6: {
+        linkText: 'Контакты',
+        linkUrl: '#'
+    }
+};
+
+// Переменные header
+let header = document.createElement('header');
 let headerContainer = document.createElement('div');
-let headerLogo = document.createElement('div');
-let headerImg = document.createElement('img');
-let headerNav = document.createElement('div');
-
-let headerPopup = document.createElement('div');
-let headerPopupWrapper = document.createElement('div');
-let headerPopupTitle = document.createElement('h1');
-let headerPopupText = document.createElement('p');
-let headerPopupClose = document.createElement('button');
-let headerPopupWrapperBtn = document.createElement('div');
-let headerPopupButton1 = document.createElement('button');
-let headerPopupButton2 = document.createElement('button');
-
-
-let headerNavList = document.createElement('ul');
-let headerNavItem = document.createElement('li');
-let headerNavLink1 = document.createElement('a');
-let headerNavLink2 = document.createElement('a');
-let headerNavLink3 = document.createElement('a');
+let headerWrapper = document.createElement('div');
 let headerButton = document.createElement('button');
+let headerNav = document.createElement('nav');
 
-
-
-// Подключение
-
+// Подключение header
 document.body.appendChild(header);
-
 header.appendChild(headerContainer);
-headerContainer.appendChild(headerLogo);
-headerLogo.appendChild(headerImg);
-headerContainer.appendChild(headerNav);
+headerContainer.appendChild(headerWrapper);
+headerWrapper.appendChild(headerButton);
+headerWrapper.appendChild(headerNav);
 
-headerNav.appendChild(headerNavList);
-headerNav.appendChild(headerButton);
-headerNav.appendChild(headerPopup);
-
-headerPopup.appendChild(headerPopupWrapper);
-headerPopupWrapper.appendChild(headerPopupTitle);
-headerPopupWrapper.appendChild(headerPopupText);
-headerPopupWrapper.appendChild(headerPopupWrapperBtn);
-headerPopupWrapper.appendChild(headerPopupClose);
-
-headerPopupWrapperBtn.appendChild(headerPopupButton1);
-headerPopupWrapperBtn.appendChild(headerPopupButton2);
-
-headerNavList.appendChild(headerNavItem);
-headerNavItem.appendChild(headerNavLink1);
-headerNavItem.appendChild(headerNavLink2);
-
-
-// Стили header классов, атрибуты
-
+// Установка стилей классов, атрибутов header
 header.setAttribute('class', 'header');
 headerContainer.setAttribute('class', 'header__container');
-headerLogo.setAttribute('class', 'header__logo');
-headerImg.setAttribute('src', './img/png/logo.png');
+headerWrapper.setAttribute('class', 'header__wrapper');
+
+headerButton.setAttribute('class', 'header__button');
+headerButton.textContent = headerBtn.text;
+
 headerNav.setAttribute('class', 'header__nav');
 
-headerPopup.setAttribute('class', 'header__info');
-headerPopupClose.setAttribute('type', 'button');
-headerPopupClose.setAttribute('class', 'header__close');
-headerPopupClose.textContent = 'X';
-headerPopupClose.addEventListener('click', function() {
-    headerPopup.classList.remove('header__open');
-});
+// Создание и добавление ссылок в навигацию
 
-// let headerPopupButtons = document.querySelectorAll('.header__popup-button');
+for (let linkItem in headerLinks) {
+    let currentLink = headerLinks[linkItem];
 
-//     for (let headerButton of headerPopupButtons) {
-//         headerButton.addEventListener('click', function() {
-//             for (let btn of headerPopupButtons) {
-//                 btn.classList.remove('header__popup-button--active');
-//             }
-//             headerButton.classList.add('header__popup-button--active');
-//         });
-//     }
+    let headerLink = document.createElement('a');
+    headerLink.textContent = currentLink.linkText;
+    headerLink.setAttribute('href', currentLink.linkUrl);
+
+    headerNav.appendChild(headerLink); 
+}
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    let headerPopupButtons = document.querySelectorAll('.header__popup-button');
+// Catalog !!!
 
-    for (let headerButton of headerPopupButtons) {
-        headerButton.addEventListener('click', function() {
-            for (let btn of headerPopupButtons) {
-                btn.classList.remove('header__popup-button--active');
-            }
-            headerButton.classList.add('header__popup-button--active');
-        });
-    }
-});
+// Объекты  Catalog
 
+// let CatalogTitle = {
+//     title: 'Каталог'
+// }
 
-headerPopupWrapperBtn.setAttribute('class', 'header__popup-wrapper-button');
-headerPopupButton1.setAttribute('type', 'button');
-headerPopupButton1.setAttribute('class', 'header__popup-button');
-headerPopupButton1.textContent = 'Кнопка 1'
-headerPopupButton2.setAttribute('type', 'button');
-headerPopupButton2.setAttribute('class', 'header__popup-button');
-headerPopupButton2.textContent = 'Кнопка 2';
+// // Переменные Catalog
 
+// let main = document.createElement('main');
+// let sectionCatalog = document.createElement('div');
+// let sectionCatalogContainer = document.createElement('div');
+// let sectionCatalogСontent = document.createElement('div');
+// let sectionCatalogTitle = document.createElement('h2');
 
+// // Подключение Catalog
 
-headerPopupWrapper.setAttribute('class', 'header__popup-wrapper');
-headerPopupTitle.setAttribute('class', 'header__popup-title');
-headerPopupTitle.textContent = 'Заголовок';
-headerPopupText.setAttribute('class', 'header__popup-text');
-headerPopupText.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo dolores doloribus necessitatibus, eum odio a debitis, consequuntur fugit similique cupiditate commodi recusandae accusantium id modi tempora iste ab omnis assumenda ipsam impedit cum facilis soluta! Atque quis doloribus exercitationem qui quam ullam provident odit enim accusantium molestiae nostrum deleniti, quidem culpa eius distinctio a numquam!';
+// document.body.appendChild(main);
 
-headerButton.setAttribute('type', 'button');
-headerButton.setAttribute('class', 'header__button');
-headerButton.textContent = 'кнопка';
-headerButton.addEventListener('click', function() {
-    headerPopup.classList.add('header__open');
-});
+// main.appendChild(sectionCatalog);
 
-headerNavItem.setAttribute('class', 'header__nav-item');
-
-headerNavLink1.setAttribute('class', 'header__link');
-headerNavLink1.setAttribute('href', 'https://www.youtube.com/watch?v=S2-31A3nqKQ&ab_channel=PeterCetera-Topic');
-headerNavLink1.textContent = 'Видео';
-
-headerNavLink2.setAttribute('class', 'header__link');
-headerNavLink2.setAttribute('href', 'https://www.youtube.com/watch?v=_91hNV6vuBY&ab_channel=BruceSpringsteenVEVO');
-headerNavLink2.textContent = 'Другое видео';
-
-// main
-
-let main = document.createElement('main');
-let sectionInfo = document.createElement('div');
-let sectionInfoContainer = document.createElement('div');
-let sectionInfoTitle = document.createElement('h2');
-let sectionInfoContent = document.createElement('div');
+// sectionCatalog.appendChild(sectionCatalogContainer);
+// sectionCatalogContainer.appendChild(sectionCatalogСontent);
+// sectionCatalogСontent.appendChild(sectionCatalogTitle);
 
 
-// Подключение
+// // Стили классов, атрибуты Catalog
 
-document.body.appendChild(main);
+// sectionCatalog.setAttribute('class', 'section-catalog');
+// sectionCatalogContainer.setAttribute('class', 'section-catalog__container');
+// sectionCatalogСontent.setAttribute('class', 'section-catalog__content');
 
-main.appendChild(sectionInfo);
-sectionInfo.appendChild(sectionInfoContainer);
-sectionInfoContainer.appendChild(sectionInfoTitle);
-sectionInfoContainer.appendChild(sectionInfoContent);
+// sectionCatalogTitle.setAttribute('class', 'section-catalog__title');
+// sectionCatalogTitle.textContent = CatalogTitle.title;
 
 
-// Стили section классов, атрибуты
 
-sectionInfo.setAttribute('class', 'section-information');
-sectionInfoContainer.setAttribute('class', 'section-information__contrainer');
-sectionInfoTitle.setAttribute('class', 'section-information__title');
-sectionInfoTitle.textContent = 'Заголовок';
-sectionInfoContent.setAttribute('class', 'section-information__content');
