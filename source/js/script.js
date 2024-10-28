32e499b88c0004149d897085e1e8182f4a25931f
Edmond1844 <pavel.rudenko1844@gmail.com>
2024-10-28 20:33:56 +0300

    сделал секцию intro

--- source/js/script.js
+++ source/js/script.js
@@ -90,6 +90,62 @@ for (let linkItem in headerLinks) {
     headerNavItem.setAttribute('class', 'header__nav-item');
 }
 
+// main 
+
+let main = document.createElement('main');
+
+document.body.appendChild(main);
+
+// Объекты  section-intro 
+let introText = {
+    textTitle: 'Печи и камины  под ключ в Санкт-Петербурге',
+    textSubtitle: 'Поможем подобрать печь или камин в ваш дом и возьмем все заботы по установке на себя',
+    textBtn: 'Получить консультацию'
+}
+
+// Переменные section-intro 
+
+let sectionIntro = document.createElement('section');
+let sectionIntroContainer = document.createElement('div');
+let sectionIntroWrapper = document.createElement('div');
+let sectionIntroTitle = document.createElement('h1');
+let sectionIntroText = document.createElement('p');
+let sectionIntroBtn = document.createElement('button');
+
+// Подключение section-intro 
+sectionIntro.appendChild(sectionIntroContainer);
+sectionIntroContainer.appendChild(sectionIntroWrapper);
+
+sectionIntroWrapper.appendChild(sectionIntroTitle);
+sectionIntroWrapper.appendChild(sectionIntroText);
+sectionIntroWrapper.appendChild(sectionIntroBtn);
+
+// Стили классов, атрибуты section-intro 
+
+sectionIntro.setAttribute('class', 'section-intro');
+sectionIntroContainer.setAttribute('class', 'section-intro__container');
+
+sectionIntroWrapper.setAttribute('class', 'section-intro__wrapper');
+
+sectionIntroTitle.setAttribute('class', 'section-intro__title');
+sectionIntroTitle.textContent = introText.textTitle;
+
+sectionIntroText.setAttribute('class', 'section-intro__subtitle');
+sectionIntroText.textContent = introText.textSubtitle;
+
+sectionIntroBtn.setAttribute('class', 'section-intro__btn');
+sectionIntroBtn.textContent = introText.textBtn;
+
+// Подключение секций к main 
+
+main.appendChild(sectionIntro);
+
+
+
+
+
+
+
 // Catalog !!!
 
 // Объекты  Catalog
