312567e482fa195a5f34d1e2877cb7185b2af4e2
Edmond1844 <pavel.rudenko1844@gmail.com>
2024-10-31 22:44:47 +0300

    Делаю header

--- source/js/script.js
+++ source/js/script.js
@@ -1,115 +1,171 @@
 // header !!!
 
 // Объекты header
-let headerBtn = {
-    text: 'Каталог'
-};
+let objectNamesHeader = {
+    typeBtn: 'button',
+    text: 'Каталог',
 
-let headerLinks = {
-    link1: {
-        linkText: 'Дымоходы',
-        linkUrl: '#'
-    },
-    link2: {
-        linkText: 'Монтаж',
-        linkUrl: '#'
-    },
-    link3: {
-        linkText: 'Портфолио',
-        linkUrl: '#'
-    },
-    link4: {
-        linkText: 'Доставка и оплата',
-        linkUrl: '#'
-    },
-    link5: {
-        linkText: 'Отзывы',
-        linkUrl: '#'
-    },
-    link6: {
-        linkText: 'Контакты',
-        linkUrl: '#'
-    }
-};
-
-let headerNumberLink = {
+    // Объекты для ссылок Header 
     textLink: '+7 (812) 924-66-61',
-    urlLink: 'tel:78129246661'
-}
+    urlLink: 'tel:78129246661',
+
+    // Объекты для img Header logo
+    srcLogo: './img/logo.png',
+    altText: 'Логотип',
+
+    // Объекты для input header
+    placeholder: 'Искать...',
+    type: 'text',
+};
+
+// Ссылки 
+let headerLinks = {
+    link1: { linkText: 'Дымоходы', linkUrl: '#' },
+    link2: { linkText: 'Монтаж', linkUrl: '#', isImportant: true },
+    link3: { linkText: 'Портфолио', linkUrl: '#', isImportant: false },
+    link4: { linkText: 'Доставка и оплата', linkUrl: '#' },
+    link5: { linkText: 'Отзывы', linkUrl: '#' },
+    link6: { linkText: 'Контакты', linkUrl: '#' }
+};
+
+// Кнопки 
+let headerButtons = {
+    btn1: { type: 'button', xlink: './img/sprite.svg#icon-basket' },
+    btn2: { type: 'button', xlink: '#' }, 
+    btn3: { type: 'button', xlink: '#' } 
+};
 
 // Переменные header
 let header = document.createElement('header');
 let headerContainer = document.createElement('div');
 let headerWrapper = document.createElement('div');
+let headerWrapperSearch = document.createElement('div');
+let headerLogo = document.createElement('img');
+let headerForm = document.createElement('form');
+let headerInput = document.createElement('input');
+let headerBtnList = document.createElement('ul');
 let headerLinksWrapper = document.createElement('div');
-let headerButton = document.createElement('button');
+let headerButtonCatalog = document.createElement('button');
 let headerNav = document.createElement('nav');
 let headerNavList = document.createElement('ul');
 let headerNumber = document.createElement('a');
 
-// Подключение header
+// Подключение к header
 document.body.appendChild(header);
 header.appendChild(headerContainer);
 headerContainer.appendChild(headerWrapper);
-headerWrapper.appendChild(headerLinksWrapper);
 
-headerLinksWrapper.appendChild(headerButton);
+headerWrapper.appendChild(headerWrapperSearch);
+headerWrapperSearch.appendChild(headerLogo);
+headerWrapperSearch.appendChild(headerForm);
+headerForm.appendChild(headerInput);
+headerWrapperSearch.appendChild(headerBtnList);
+
+headerWrapper.appendChild(headerLinksWrapper);
+headerLinksWrapper.appendChild(headerButtonCatalog);
 headerLinksWrapper.appendChild(headerNav);
 headerNav.appendChild(headerNavList);
 headerLinksWrapper.appendChild(headerNumber);
-
-
 
 // Установка стилей классов, атрибутов header
 header.setAttribute('class', 'header');
 headerContainer.setAttribute('class', 'header__container');
 headerWrapper.setAttribute('class', 'header__wrapper');
-
 headerLinksWrapper.setAttribute('class', 'header__links-wrapper');
+headerWrapperSearch.setAttribute('class', 'header__search-wrapper');
 
-headerButton.setAttribute('class', 'header__button');
-headerButton.textContent = headerBtn.text;
+// header logo
+headerLogo.setAttribute('class', 'header__img');
+headerLogo.setAttribute('alt', objectNamesHeader.altText);
+headerLogo.setAttribute('src', objectNamesHeader.srcLogo);
 
+// form header
+headerForm.setAttribute('class', 'header__form');
+
+// header input
+headerInput.setAttribute('class', 'header__input');
+headerInput.setAttribute('placeholder', objectNamesHeader.placeholder);
+headerInput.setAttribute('type', objectNamesHeader.type);
+
+// header button list 
+headerBtnList.setAttribute('class', 'header__btn-list');
+
+for (let btnItem in headerButtons) {
+    let currentBtn = headerButtons[btnItem];
+
+    let headerBtnItem = document.createElement('li');
+    let headerBtn = document.createElement('button');
+    let headerSvgIcon = document.createElement('svg');
+    let headerSvgUse = document.createElement('use');
+
+    headerBtn.setAttribute('type', currentBtn.type);
+    headerBtn.setAttribute('class', 'header__btn-item');
+
+    headerBtnList.appendChild(headerBtnItem);
+    headerBtnItem.appendChild(headerBtn);
+    headerBtn.appendChild(headerSvgIcon);
+    headerSvgIcon.appendChild(headerSvgUse);
+
+    // Установка размеров иконки
+    headerSvgIcon.setAttribute('width', '16'); 
+    headerSvgIcon.setAttribute('height', '17'); 
+    headerSvgUse.setAttribute('href', currentBtn.xlink)
+
+    // Обработчик события для кнопки
+    headerBtn.addEventListener('click', function() {
+        let headerbuttons = document.querySelectorAll('.header__btn-item'); 
+        for (let btn of headerbuttons) {
+            btn.classList.remove('header__btn-item--active'); 
+        }
+        headerBtn.classList.add('header__btn-item--active'); 
+    });
+}
+
+// header button
+headerButtonCatalog.setAttribute('class', 'header__button');
+headerButtonCatalog.textContent = objectNamesHeader.text;
+headerButtonCatalog.setAttribute('type', objectNamesHeader.typeBtn);
+
+// header nav
 headerNav.setAttribute('class', 'header__nav');
-
 headerNavList.setAttribute('class', 'header__nav-list');
-
-headerNumber.setAttribute('class', 'header__number')
-headerNumber.setAttribute('href', headerNumberLink.urlLink); 
-headerNumber.textContent = headerNumberLink.textLink; 
+headerNumber.setAttribute('class', 'header__number');
+headerNumber.setAttribute('href', objectNamesHeader.urlLink);
+headerNumber.textContent = objectNamesHeader.textLink;
 
 // Создание и добавление ссылок в навигацию
-
 for (let linkItem in headerLinks) {
     let currentLink = headerLinks[linkItem];
 
     let headerNavItem = document.createElement('li');
-
     let headerLink = document.createElement('a');
     headerLink.textContent = currentLink.linkText;
     headerLink.setAttribute('href', currentLink.linkUrl);
 
-    headerNavItem.appendChild(headerLink); 
+    headerNavItem.appendChild(headerLink);
     headerNavList.appendChild(headerNavItem);
 
     headerNavItem.setAttribute('class', 'header__nav-item');
+
+    if (currentLink.isImportant === true) { 
+        headerLink.classList.add('header--red');
+    }
 }
+
+console.log('привет');
 
 // main 
-
 let main = document.createElement('main');
-
 document.body.appendChild(main);
 
-// Объекты  section-intro 
+// Объекты section-intro 
 let introText = {
-    textTitle: 'Печи и камины  под ключ в Санкт-Петербурге',
+    textTitle: 'Печи и камины под ключ в Санкт-Петербурге',
     textSubtitle: 'Поможем подобрать печь или камин в ваш дом и возьмем все заботы по установке на себя',
     textBtn: 'Получить консультацию'
-}
+};
 
 // Переменные section-intro 
-
 let sectionIntro = document.createElement('section');
 let sectionIntroContainer = document.createElement('div');
 let sectionIntroWrapper = document.createElement('div');
@@ -126,10 +182,8 @@ sectionIntroWrapper.appendChild(sectionIntroText);
 sectionIntroWrapper.appendChild(sectionIntroBtn);
 
 // Стили классов, атрибуты section-intro 
-
 sectionIntro.setAttribute('class', 'section-intro');
 sectionIntroContainer.setAttribute('class', 'section-intro__container');
-
 sectionIntroWrapper.setAttribute('class', 'section-intro__wrapper');
 
 sectionIntroTitle.setAttribute('class', 'section-intro__title');
@@ -142,50 +196,4 @@ sectionIntroBtn.setAttribute('class', 'section-intro__btn');
 sectionIntroBtn.textContent = introText.textBtn;
 
 // Подключение секций к main 
-
 main.appendChild(sectionIntro);
-
-
-
-
-
-
-
-// Catalog !!!
-
-// Объекты  Catalog
-
-// let CatalogTitle = {
-//     title: 'Каталог'
-// }
-
-// // Переменные Catalog
-
-// let main = document.createElement('main');
-// let sectionCatalog = document.createElement('div');
-// let sectionCatalogContainer = document.createElement('div');
-// let sectionCatalogСontent = document.createElement('div');
-// let sectionCatalogTitle = document.createElement('h2');
-
-// // Подключение Catalog
-
-// document.body.appendChild(main);
-
-// main.appendChild(sectionCatalog);
-
-// sectionCatalog.appendChild(sectionCatalogContainer);
-// sectionCatalogContainer.appendChild(sectionCatalogСontent);
-// sectionCatalogСontent.appendChild(sectionCatalogTitle);
-
-
-// // Стили классов, атрибуты Catalog
-
-// sectionCatalog.setAttribute('class', 'section-catalog');
-// sectionCatalogContainer.setAttribute('class', 'section-catalog__container');
-// sectionCatalogСontent.setAttribute('class', 'section-catalog__content');
-
-// sectionCatalogTitle.setAttribute('class', 'section-catalog__title');
-// sectionCatalogTitle.textContent = CatalogTitle.title;
-
-
-
